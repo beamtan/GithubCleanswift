1b92b16f02cc17139909f20c3c45c69419bf9b05
chayakan tangsanga <chayakan.t@arise.tech>
2023-08-30 13:32:40 +0700

    Initial Commit

---
+++ GIthubListClean/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GIthubListClean
+//
+//  Created by A667243 A667243 on 30/8/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
