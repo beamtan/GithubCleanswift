168050fd6e87c84bde68bd19bc1acd1cf8624572
beamtan <beamtantan@gmail.com>
2023-09-13 19:50:43 +0700

    Add navigation for pages

--- GIthubListClean/ViewController.swift
+++ GIthubListClean/ViewController.swift
@@ -17,10 +17,47 @@ import UIKit
 //let loginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
 //self.navigationController?.pushViewController(loginVC, animated: true)
 
-class MainViewController: UIViewController {
+class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello world")
     }
+    
+    @IBAction func changeScene(_ sender: UIButton) {
+//        // Load the "Github" storyboard
+//        let storyboard = UIStoryboard(name: "Github", bundle: nil)
+//
+//        if let githubViewController = storyboard.instantiateViewController(withIdentifier: "GithubViewController") as? GithubViewController {
+//            // Create a UINavigationController and set your view controller as its root
+//            let navigationController = UINavigationController(rootViewController: githubViewController)
+//
+//            if let existingNavController = self.navigationController {
+//                // Push the navigation controller onto the navigation stack if self has a navigation controller
+//                existingNavController.pushViewController(githubViewController, animated: true)
+//            } else {
+//                // Otherwise, present the navigation controller modally
+//                self.present(navigationController, animated: true, completion: nil)
+//            }
+//        } else {
+//            // Handle any errors or issues with the storyboard or view controller instantiation
+//            print("Failed to instantiate GithubViewController from storyboard")
+//        }
+        
+        let storyboard = UIStoryboard(name: "Github", bundle: nil)
+        
+        if let githubViewController = storyboard.instantiateViewController(withIdentifier: "GithubViewController") as? GithubViewController {
+            // Create a UINavigationController
+            let navigationController = UINavigationController(rootViewController: githubViewController)
+            
+            // Set the navigation controller as the root view controller of the window
+            UIApplication.shared.windows.first?.rootViewController = navigationController
+            
+            print("Pushed")
+        } else {
+            // Handle any errors or issues with the storyboard or view controller instantiation
+            print("Failed to instantiate GithubViewController from storyboard")
+        }
+    }
+    
 }
